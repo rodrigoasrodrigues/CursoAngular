04d8674f48f5b60246f89f1098b3a1d8bae42de9
Rodrigo Rodrigues <serdasteclas@gmail.com>
2018-08-29 22:06:05 -0300

    estrutura basica de rotas

---
+++ app-angular4/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<menu></menu>'
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-angular4';
+}
---
+++ app-angular4/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+//especificacao da rota deve vir antes dos componentes
+import { appRoutes } from './rotas/app.routes'
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, MenuComponent, HomeComponent, CadastroComponent, NotfoundComponent
+  ],
+  imports: [
+    BrowserModule, RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
